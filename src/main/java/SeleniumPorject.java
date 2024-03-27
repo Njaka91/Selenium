caa586885c59ba4fe0182002ec0e146f03a99133
Njaka91 <andriaravelo91@gmail.com>
2024-03-27 13:24:28 +0300

    second commit

--- src/main/java/SeleniumPorject.java
+++ src/main/java/SeleniumPorject.java
@@ -6,21 +6,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumPorject {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
+        driver.get("https://google.com/");
 
         // Rechercher "inclusiv academy"
         WebElement searchBox = driver.findElement(By.name("q"));
         searchBox.sendKeys("inclusiv academy");
         searchBox.submit();
 
-        // Attendre un peu pour laisser le temps à la page de se charger
+        // Attendre 3 secondes  pour chrager la page
         try {
-            Thread.sleep(2000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        // Cliquer sur le premier résultat
+        // Cliquer sur le premier lien
         WebElement firstResult = driver.findElement(By.cssSelector("h3"));
         firstResult.click();
 
